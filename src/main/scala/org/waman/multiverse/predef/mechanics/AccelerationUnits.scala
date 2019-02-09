5b4d045c5710e16b6c9f1ba6a6dc7aab7a187d30
waman <waman10da@gmail.com>
2019-02-09 10:41:11 +0900

    Add mass units

---
+++ src/main/scala/org/waman/multiverse/predef/mechanics/AccelerationUnits.scala
@@ -0,0 +1,9 @@
+package org.waman.multiverse.predef.mechanics
+
+import spire.implicits._
+import org.waman.multiverse.units.basic.SimpleVelocityUnit
+
+trait AccelerationUnits{
+
+  case object g0 extends SimpleVelocityUnit("standard gravity", r"9.80665")
+}
