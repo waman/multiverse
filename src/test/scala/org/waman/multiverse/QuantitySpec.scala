d99503f41593491e23fc3a898c6da3b55a204adf
waman <waman10da@gmail.com>
2019-02-16 11:38:24 +0900

    Enable acceleration unit to denote by ^ op (like m/s^2)

--- src/test/scala/org/waman/multiverse/QuantitySpec.scala
+++ src/test/scala/org/waman/multiverse/QuantitySpec.scala
@@ -1,7 +1,7 @@
 package org.waman.multiverse
 
-import org.waman.multiverse.implicits._
 import spire.implicits._
+import org.waman.multiverse.implicits._
 import org.waman.multiverse.predef.BasicUnits._
 
 class QuantitySpec extends MultiverseCustomSpec{
