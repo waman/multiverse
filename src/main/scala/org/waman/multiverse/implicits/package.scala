9bae5f06834ea9148d280026e410e8dad1904fe3
waman <waman10da@gmail.com>
2019-02-16 11:40:20 +0900

    Change QuantityFactory class to implict one

--- src/main/scala/org/waman/multiverse/implicits/package.scala
+++ src/main/scala/org/waman/multiverse/implicits/package.scala
@@ -1,13 +1,24 @@
 package org.waman.multiverse
 
+import org.waman.multiverse.units.basic._
+import org.waman.multiverse.units.mechanics.{Acceleration, AccelerationUnit}
 import spire.math.{Fractional, Real, SafeLong}
 
 import scala.language.implicitConversions
 
 package object implicits {
 
-  implicit def convertFractionalToQuantityFactory[A: Fractional](value: A): QuantityFactory[A] =
-    new QuantityFactory(value)
+  implicit class QuantityFactory[A: Fractional](val value: A){
+
+    def apply(unit: LengthUnit): Length[A] = new Length(value, unit)
+    def apply(unit: MassUnit): Mass[A] = new Mass(value, unit)
+    def apply(unit: TimeUnit): Time[A] = new Time(value, unit)
+    def apply(unit: VelocityUnit): Velocity[A] = new Velocity(value, unit)
+    def apply(unit: AccelerationUnit): Acceleration[A] = new Acceleration(value, unit)
+  }
+
+//  implicit def convertFractionalToQuantityFactory[A: Fractional](value: A): QuantityFactory[A] =
+//    new QuantityFactory(value)
 
   // Integral value (like 1(m), not 1.0(m)) create a Quantity[Real] instance
   implicit def convertIntToQuantityFactory(value: Int): QuantityFactory[Real] =
