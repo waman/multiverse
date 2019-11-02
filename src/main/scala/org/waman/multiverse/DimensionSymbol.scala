bfd2a4806e643fd62bfa30f99a777ae8e50f602f
waman <waman10da@gmail.com>
2019-11-02 13:10:52 +0900

    Add isEquivalent method to LinearUnit to evaluate equivalency (dimension and interval are the same values between two units) and DimensionSymbol.toStringWithXxx method to make dimension map more readable

---
+++ src/main/scala/org/waman/multiverse/DimensionSymbol.scala
@@ -0,0 +1,43 @@
+package org.waman.multiverse
+
+sealed abstract class DimensionSymbol(val unit: String)
+
+object DimensionSymbol{
+  final case object M extends DimensionSymbol("kg")
+  final case object L extends DimensionSymbol("m")
+  final case object T extends DimensionSymbol("s")
+  final case object I extends DimensionSymbol("A")
+  final case object Θ extends DimensionSymbol("K")
+  final case object N extends DimensionSymbol("mol")
+  final case object J extends DimensionSymbol("cd")
+
+  def values: Seq[DimensionSymbol] = Seq(M, L,  T, I, Θ, N, J)
+
+  def valueOf(s: String): Option[DimensionSymbol] = s match {
+    case "M" => Some(M)
+    case "L" => Some(L)
+    case "T" => Some(T)
+    case "I" => Some(I)
+    case "Θ" => Some(Θ)
+    case "N" => Some(N)
+    case "J" => Some(J)
+    case _ => None
+  }
+
+  private val uppers = "⁰¹²³⁴⁵⁶⁷⁸⁹⁻"
+
+  private def toUppers(c: Char): Char = c match {
+    case '-' => uppers.charAt(10)
+    case n => uppers.charAt(n - '0')
+  }
+
+  def toStringWithUnit(d: Map[DimensionSymbol, Int]): String = toString(d, _.unit, " ")
+
+  def toStringWithSymbol(d: Map[DimensionSymbol, Int]): String = toString(d, _.toString, "")
+
+  private def toString(d: Map[DimensionSymbol, Int], f: DimensionSymbol => String, sep: String): String =
+    values.map(s => (s, d(s))).filter(_._2 != 0).map{
+      case (s, 1) => f(s)
+      case (s, n) => f(s) + n.toString.map(toUppers).mkString("")
+    }.mkString(sep)
+}
